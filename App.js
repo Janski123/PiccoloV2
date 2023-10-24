a51f338050deccea6b00ba36249d955757366f05
Janski123 <varjonvelho@gmail.com>
2023-10-24 22:09:33 +0300

    Added settings page and ability start the game.

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import StartGame from './components/StartGame'
 import HomeScreen from './components/HomeScreen'
-
+import Game from './components/Game'
 const Stack = createStackNavigator();
 
 
@@ -13,6 +13,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="PelinAloitus" component={StartGame} />
+        <Stack.Screen name="Game" component={Game} />
       </Stack.Navigator>
     </NavigationContainer>
     
